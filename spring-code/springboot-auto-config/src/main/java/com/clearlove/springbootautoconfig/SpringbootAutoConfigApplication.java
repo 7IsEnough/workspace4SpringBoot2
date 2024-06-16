120fd001f11f409b9dcbe90a5f6be74ddfba00b2
Chen Zhenhuan <976949689@qq.com>
2024-06-16 23:29:51 +0800

    2024年6月16日23:29:45

---
+++ spring-code/springboot-auto-config/src/main/java/com/clearlove/springbootautoconfig/SpringbootAutoConfigApplication.java
@@ -0,0 +1,18 @@
+package com.clearlove.springbootautoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringbootAutoConfigApplication {
+
+  public static void main(String[] args) {
+    ApplicationContext context = SpringApplication.run(
+        SpringbootAutoConfigApplication.class, args);
+
+    System.out.println(context.getBean("dispatcherServlet"));
+    System.out.println(context.getBean("province"));
+  }
+}
---
+++ spring-code/springboot-auto-config/src/test/java/com/clearlove/springbootautoconfig/SpringbootAutoConfigApplicationTests.java
@@ -0,0 +1,11 @@
+package com.clearlove.springbootautoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAutoConfigApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
