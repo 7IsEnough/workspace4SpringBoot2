70daaa8bd1fc07fb0fb9592f83a54f3c7e9e0524
Chen Zhenhuan <976949689@qq.com>
2024-06-17 16:18:08 +0800

    2024年6月17日16:18:03

---
+++ spring-code/springboot-profiles/src/test/java/com/clearlove/springbootprofiles/SpringbootProfilesApplicationTests.java
@@ -0,0 +1,11 @@
+package com.clearlove.springbootprofiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProfilesApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
